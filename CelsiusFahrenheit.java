491119e9b1f0f2f411662cc4d3fb45e81f31c624
wimmossige <wimmossige@outlook.com>
2017-10-15 21:04:12 +0200

    lagt til fil

---
+++ CelsiusFahrenheit.java
@@ -0,0 +1,61 @@
+/*
+* Wim Felix Mossige
+* Modul 6
+* Celsius Fahrenheit
+ */
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+public class CelsiusFahrenheit {
+	
+	
+	public static void main(String[] args) {
+
+		String celsiusH = 		"Celsius";
+		String fahrenheitH = 	"Fahrenheit";
+		String rom = "          ";
+		String heading = (celsiusH + rom + fahrenheitH + rom + "|" + rom + fahrenheitH + rom + celsiusH);
+		System.out.print(heading + "\n");
+		for (int i = 1; i <= heading.length(); i++)
+			System.out.print("-");
+		System.out.print("\n");
+
+
+
+
+		DecimalFormat df = new DecimalFormat("#.0");
+		
+		double celsius = 40, fahrenheit = 120, fahrenheitConverted, celsiusConverted;
+
+		
+		
+		for (int i = 0 ; i < 10; i++) {
+			celsius = 40-i;
+            fahrenheit = 120-10*i;
+			celsiusConverted = celsiusToFahrenheit(celsius);
+			fahrenheitConverted = fahrenHeitToCelsius(fahrenheit);
+
+			System.out.print(df.format(celsius) + rom +  df.format(celsiusConverted) + rom + "|" + rom + df.format(fahrenheit) + rom + df.format(fahrenheitConverted)+"\n");
+
+			
+		}
+	}
+
+
+	public static double celsiusToFahrenheit(double x) {
+		double y = (9.0/5) * x + 32;
+		return y;
+	}
+	public static double fahrenHeitToCelsius(double x) {
+		double y = (5.0/9) * (x - 32);
+		return y;
+	}
+
+
+
+
+
+
+
+
+}
